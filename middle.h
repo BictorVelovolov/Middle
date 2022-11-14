93d16b7b74ef488ddfe80eb08927e325e264b115
BictorVelovolov <112112051+BictorVelovolov@users.noreply.github.com>
2022-11-14 18:34:41 +0300

    Update middle.h

--- middle.h
+++ middle.h
@@ -1,21 +1,21 @@
 #ifndef MIDDLE_H_INCLUDED
 #define MIDDLE_H_INCLUDED
 
-int itc_num_print (int a);
-int itc_len_num(long a);
-int itc_sum_num(long a);
-int itc_max_num(long a);
-long itc_multi_num(long a);
-int itc_min_num(long a);
-int itc_rev_num(long a);
-int itc_null_count(long a);
-bool itc_mirror_num(long a);
-long itc_bin_num(long a);
-int itc_mirror_count(long a);
-int itc_second_max_num(long a);
-int itc_second_simple_max_num(long a);
-long itc_oct_num(long a);
-int itc_rev_bin_num(long a);
-int itc_rev_oct_num(long a);
+void itc_num_print(int a); //Печатает число
+int itc_len_num(long long a); //Печатает количество разрядов в числе
+int itc_sum_num(long long a); //Печатает сумму цифр числа
+int itc_max_num(long long a); //Макс. цифра в числе
+long long itc_multi_num(long long a); //Произведение цифр в числе
+int itc_min_num(long long a); //минимальная цифра
+int itc_rev_num(long long a); //отзеркаливает число
+int itc_null_count(long long a); //Счёт нулей
+bool itc_mirror_num(long long a); //Чётное/нечётное число
+long long itc_bin_num(long long a); //перевод числа в двоичку
+int itc_mirror_count(long long a);
+int itc_second_max_num(long long a);
+int itc_second_simple_max_num(long long a);
+long long itc_oct_num(long long a);
+int itc_rev_bin_num(long long a);
+int itc_rev_oct_num(long long a);
 
 #endif // MIDDLE_H_INCLUDED
