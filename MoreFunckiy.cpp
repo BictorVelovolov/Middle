00d7a30e9ebbe0a71dc163e7e54a152382b65aee
BictorVelovolov <112112051+BictorVelovolov@users.noreply.github.com>
2022-11-14 18:31:53 +0300

    Update MoreFunckiy.cpp

--- MoreFunckiy.cpp
+++ MoreFunckiy.cpp
@@ -1,30 +1,32 @@
 #include<iostream>
-#include"Middle.h"
+#include"middle.h"
 using namespace std;
 
- int itc_min_num(long a) //Number 6
+ int itc_min_num(long long a) //Number 6
  {
-     int g, h;
+     int g, h, l;
      g = 10;
+     if (a == 0)
+        return 0;
      if (a > 0)
     {
-        while (a>0)
+        while (a > 0)
         {
-            h=a%10;
-            if (h<g)
+            h = a % 10;
+            if (h < g)
             {
             g = h;
             }
             a = a / 10;
         }
     }
-    else
+    if (a < 0)
     {
         a = a * (-1);
         while (a>0)
         {
             h=a%10;
-            if (h<g)
+            if (h < g)
             {
             g = h;
             }
@@ -33,7 +35,7 @@ using namespace std;
     }
     return g;
  }
-int itc_rev_num(long a) //Number 7
+int itc_rev_num(long long a) //Number 7
 {
     int g, rc;
     rc = 0;
@@ -55,10 +57,12 @@ int itc_rev_num(long a) //Number 7
             rc = rc * 10 + g;
             a = a / 10;
         }
+        rc = rc * (-1);
     }
+
     return rc;
 }
- int itc_null_count(long a) //Number 8
+ int itc_null_count(long long a) //Number 8
  {
      int g, h;
      h = 0;
@@ -88,42 +92,36 @@ int itc_rev_num(long a) //Number 7
      return h;
  }
 
- bool itc_mirror_num(long a) // Number 9
+ long long perevorot(long long i){
+     int g, nch;
+     if (i < 0)
+        i = i * (-1);
+      while (i != 0)
+        {
+            g = i % 10;
+            nch = nch*10 + g;
+            i = i / 10;
+        }
+     return nch;
+ }
+
+ bool itc_mirror_num(long long a) // Number 9
 {
-    long i, nch, g;
+    long long i, nch, g;
     i = a;
-    nch = 0;
-    if (a > 0)
-    {
-        while (i != 0)
-        {
-            g = i % 10;
-            nch = nch*10 + g;
-            i = i / 10;
-        }
-    }
-    else
-    {
-        a = a * (-1);
-        while (i != 0)
-        {
-            g = i % 10;
-            nch = nch*10 + g;
-            i = i / 10;
-        }
-    }
-    if (nch == a * (-1))
+    nch = perevorot(a);
+    if (nch == a * (-1) || nch == a)
         return 1;
     return 0;
 }
- int itc_mirror_count(long a) //Number 10
+ int itc_mirror_count(long long a) //Number 10
  {
      int b, i;
      if (a > 0)
      {
         for (i = 1; i < a; i++)
         {
-            if (itc_mirror_num(i) == 1)
+            if (itc_mirror_num(i))
                 b = b + 1;
         }
      }
