f7025d5f3028f6dd445d66119eaebe1ce77321fc
BictorVelovolov <112112051+BictorVelovolov@users.noreply.github.com>
2022-11-15 22:42:47 +0300

    Update funkcii3.cpp

--- funkcii3.cpp
+++ funkcii3.cpp
@@ -1,7 +1,12 @@
+#include<iostream>
+#include"middle.h"
+using namespace std;
+
 int itc_second_max_num(long long a) //Number 11
 {
-    int d, h, g;
+    int d, h, g, i;
     h = 0;
+    i = 0;
     if (a / 10 != 0)
     {
         if (a < 0)
@@ -17,12 +22,14 @@ int itc_second_max_num(long long a) //Number 11
                     h = d;
                 }
                 a = a / 10;
+                if (d == g)
+                    i = i + 1;
             }
-            if (h == g)
-                return -1;
+            if (i > 1)
+                return g;
         return h;
 
-    }
+        }
     }
     return -1;
 }
@@ -34,8 +41,8 @@ int itc_second_max_num(long long a) //Number 11
     i = 0;
     if (a / 10 !=0)
     {
-        if (a > 0)
-        {
+        if (a < 0)
+            a = a*(-1);
             g = itc_max_num(a);
             while (a > 0)
             {
@@ -49,24 +56,6 @@ int itc_second_max_num(long long a) //Number 11
                 if (d == g)
                     i = i + 1;
             }
-        }
-        else
-        {
-            a = a * (-1);
-           g = itc_max_num(a);
-           while (a > 0)
-           {
-               d = a % 10;
-              if (g > d)
-            {
-                  if (d > h)
-                    h = d;
-            }
-            a = a / 10;
-            }
-            if (d == g)
-                i = i + 1;
-        }
             if (i > 1)
             return -1;
 
@@ -80,8 +69,8 @@ long long itc_bin_num(long long a) //Number 13
     int nch, d, i, g;
     nch = 0;
     i = 1;
-    if (a > 0)
-    {
+    if (a < 0)
+    a = a * (-1);
         while (a > 0)
         {
             d = a % 2;
@@ -90,19 +79,7 @@ long long itc_bin_num(long long a) //Number 13
             if (nch == 0)
                 i = i * 10;
         }
-    }
-    else
-    {
-        while (a > 0)
-        {
-            d = a % 2;
-            nch = nch * 10 + d;
-            a = a / 2;
-            if (nch == 0)
-                i = i * 10;
-        }
-    }
-    g = nch * i;
+    g = perevorot(nch) * i;
     return g;
 }
 long long itc_oct_num(long long a) //Number 14
@@ -110,8 +87,8 @@ long long itc_oct_num(long long a) //Number 14
     int nch, d, g, i;
     nch = 0;
     i = 1;
-    if (a > 0)
-    {
+    if (a < 0)
+    a = a * (-1);
         while (a > 0)
         {
             d = a % 8;
@@ -122,27 +99,9 @@ long long itc_oct_num(long long a) //Number 14
         }
             if (nch != 0)
             {
-            g = nch * i;
-            return itc_rev_num(g);
+            g = perevorot(nch) * i;
+            return g;
             }
-    }
-    else
-    {
-        a = a * (-1);
-        while (a > 0)
-        {
-            d = a % 8;
-            nch = nch*10 + d;
-            a = a / 8;
-            if (nch == 0)
-                i = i * 10;
-        }
-            if (nch != 0)
-            {
-            g = nch * i;
-            return itc_rev_num(g);
-            }
-    }
     return i;
 }
 
